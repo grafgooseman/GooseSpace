ee4ed937a05a1f96f364df1b4aee594910198925
Artem Gusev <artemgusev2100@gmail.com>
2023-07-15 19:01:46 -0400

    Profile editing added

--- app/dashboard/ProfileForm.tsx
+++ app/dashboard/ProfileForm.tsx
@@ -1,1 +1,78 @@
- 
+"use client";
+
+export function ProfileForm({ user }: any) {
+    const updateUser = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const body = {
+            name: formData.get("name"),
+            bio: formData.get("bio"),
+            age: formData.get("age"),
+            image: formData.get("image"),
+        };
+
+        const res = await fetch("/api/user", {
+            method: "PUT",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        // await res.json();
+    };
+
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl font-semiboldbold mb-4">Edit Your Profile</h2>
+            <form onSubmit={updateUser} className="space-y-4 flex flex-col">
+                <label htmlFor="name" className="block font-semibold">
+                    Name
+                </label>
+                <input
+                    className="border border-gray-300 rounded-md p-2"
+                    type="text"
+                    name="name"
+                    defaultValue={user?.name ?? ""}
+                />
+                <label htmlFor="bio" className="block font-semibold">
+                    Bio
+                </label>
+                <textarea
+                    name="bio"
+                    cols={30}
+                    rows={10}
+                    defaultValue={user?.bio ?? ""}
+                    className="border border-gray-300 rounded-md p-2"
+                ></textarea>
+                <label htmlFor="age" className="block font-semibold">
+                    Age
+                </label>
+                <input
+                    type="text"
+                    name="age"
+                    defaultValue={user?.age ?? 0}
+                    className="border border-gray-300 rounded-md p-2"
+                />
+                <label htmlFor="image" className="block font-semibold">
+                    Profile Image URL
+                </label>
+                <input
+                    type="text"
+                    name="image"
+                    defaultValue={user?.image ?? ""}
+                    className="border border-gray-300 rounded-md p-2 w-200"
+                />
+
+                <button
+                    className="bg-blue-500 text-white rounded-md py-2 px-4"
+                    type="submit"
+                >
+                    Save
+                </button>
+            </form>
+        </div>
+    );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,28 @@
-import { getServerSession } from 'next-auth';
-import { prisma } from '@/lib/prisma';
-import { ProfileForm } from './ProfileForm';
-import { redirect } from 'next/navigation';
-import { SignOutButton } from '@/components/buttons';
-import { authOptions } from "../api/auth/[...nextauth]/route"
-
+import { getServerSession } from "next-auth";
+import { prisma } from "@/lib/prisma";
+import { ProfileForm } from "./ProfileForm";
+import { redirect } from "next/navigation";
+import { SignOutButton } from "@/components/buttons";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Dashboard() {
     const session = await getServerSession(authOptions);
 
-    if(!session){
-        redirect('/api/auth/signin');
+    if (!session) {
+        redirect("/api/auth/signin");
     }
+
+    const currentUserEmail = session?.user?.email!;
+    const user = await prisma.user.findUnique({
+        where: {
+            email: currentUserEmail,
+        },
+    });
 
     return (
         <>
-
+            <h1 className="text-3xl font-bold m-5" >Dashboard</h1>
+            <ProfileForm user={user} />
         </>
+    );
 }
